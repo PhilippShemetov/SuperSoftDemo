7d35f10c4a7266ab8de115c861791db587b9ed92
PhilippShemetov <filshe2704@mail.ru>
2020-12-09 16:13:07 +0300

    Add project

---
+++ src/test/java/com/supersoft/projmanagment/ProjmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supersoft.projmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjmanagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

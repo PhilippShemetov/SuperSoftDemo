4fc9468039e62abdeebb5dc7c329d2117658ad8d
PhilippShemetov <filshe2704@mail.ru>
2020-12-09 16:38:51 +0300

    add gitignore

---
+++ src/main/java/com/supersoft/projmanagment/ProjmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.supersoft.projmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjmanagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjmanagmentApplication.class, args);
+    }
+
+}
